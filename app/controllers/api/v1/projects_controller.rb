6cc514f47aab143f54da6a055ee5a67a5820e36d
Alexey Korichkovskiy <johniezigger@gmail.com>
2016-01-22 19:41:27 +0200

    folding in controllers

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,44 +1,37 @@
-module  Api
-  module V1
-    class ProjectsController < ApplicationController
-      skip_before_filter :verify_authenticity_token
-      respond_to :json
-      def index
-        puts "aaa"
-        puts 
-        respond_with Project.all
-      end
-      def show 
-        respond_with(Project.find(params['id']))
-      end
-      def create 
-        puts params
-        @todo=Project.new(project_params)
-        current_user.projects<<@todo
-        if @todo.save
-            render :json => @todo
+class Api::V1::ProjectsController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token
+  respond_to :json
+
+  def index
+    respond_with Project.where
+  end
+
+  def create 
+    @todo=Project.new(project_params)
+    current_user.projects<<@todo
+    if @todo.save
+      render :json => @todo
       else
+      render json: @todo.errors
+    end
+  end
 
-        render json: @todo.errors
-      end
-      end
-      def update
-        @todo = Project.find(params[:id])
-        if @todo.update(todo_params)
-          respond_to do |format|
-            format.json{ render :json => @todo}
-          end
-        end
-      end
-      def destroy
-          respond_with Project.destroy_all({ :_id => BSON::ObjectId(params[:id])})
-      end
-
-       def project_params
-               params.require(:project).permit(:name, :deadline)
-
-        
+  def update
+    @todo = Project.find(params[:id])
+    if @todo.update(todo_params)
+      respond_to do |format|
+        format.json{ render :json => @todo}
       end
     end
   end
+
+  def destroy
+    respond_with Project.destroy_all({ :_id => BSON::ObjectId(params[:id])})
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :deadline)      
+  end
+
 end
--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,48 +1,39 @@
-module  Api
-  module V1
-    class TasksController < ApplicationController
-      skip_before_filter :verify_authenticity_token
-      respond_to :json
-      def index
-        puts "aaa"
-        puts 
-        respond_with Project.all
-      end
-      def show 
-        respond_with(Task.find(params[:id]))
-      end
-      def create
-        @project=Project.find(params[:id])
-        @task=Task.new(task_params)
-        @project.tasks<<@task
-        if @project.save
-          respond_to do |format|
-            format.json{ render :json => @project}
-          end
-        end
-      end
-      def update
-        project = Project.where("tasks._id" => BSON::ObjectId(params[:id])).first
-        task=project.tasks.find(params[:id])
-        if task.update(task_params)
-          respond_to do |format|
-            format.json{ render :json => task}
-          end
-        end
-      end
-      def destroy
-        #can't directly find task!
-        project = Project.where("tasks._id" => BSON::ObjectId(params[:id])).first
-        task=project.tasks.find(params[:id])
-         respond_with task.delete
-      end
-      protected
-      def task_params
-        params.require(:task).permit(:name)
+class Api::V1::TasksController < ApplicationController
 
-        
+  skip_before_filter :verify_authenticity_token
+  respond_to :json
+  
+  def create
+    @project=Project.find(params[:id])
+    @task=Task.new(task_params)
+    @project.tasks<<@task
+    if @project.save
+      respond_to do |format|
+        format.json{ render :json => @project}
       end
     end
   end
-end
- 
+
+  def update
+    project = Project.where("tasks._id" => BSON::ObjectId(params[:id])).first
+    task=project.tasks.find(params[:id])
+    if task.update(task_params)
+      respond_to do |format|
+        format.json{ render :json => task}
+      end
+    end
+  end
+
+  def destroy
+    #can't directly find task!
+    project = Project.where("tasks._id" => BSON::ObjectId(params[:id])).first
+    task=project.tasks.find(params[:id])
+   respond_with task.delete
+  end
+
+  protected
+
+  def task_params
+    params.require(:task).permit(:name)
+  end
+end 
