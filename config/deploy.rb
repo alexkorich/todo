297132fd529c103a816f8281305240c983342e66
Alexey Korichkovskiy <johniezigger@gmail.com>
2016-01-30 21:38:40 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 
 set :application, 'todo'
 set :repo_url, 'git@github.com:alexkorich/todo.git'
-set :branch, :tokenless
+set :branch, :master
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
